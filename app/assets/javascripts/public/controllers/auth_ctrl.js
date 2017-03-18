58cb9fb4fd3b62ffcfca625c9dd2cd612bcc0d9a
Paul Kononenko <storuky@gmail.com>
2017-03-18 18:13:24 +0300

    Fix secrets.yml

--- app/assets/javascripts/public/controllers/auth_ctrl.js
+++ app/assets/javascripts/public/controllers/auth_ctrl.js
@@ -1,8 +1,10 @@
 app.controller('AuthCtrl', ['action', 'SignService', function(action, SignService){
   var ctrl = this;
+  ctrl.user = {};
 
   action("sign", function (params) {
     ctrl.submit = function ($event) {
+      ctrl.user.action = params.type;
       SignService[params.type](ctrl.user).then(function (res) {
         if (!res.data.reload)
           gon.current_user = res.data.current_user;
